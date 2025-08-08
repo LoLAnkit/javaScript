1aa8f02d2c3f5b79a8a3fa94e1bc883bc0674e7f
Ankit Thapa <thapaankit444@gmail.com>
2025-08-08 06:43:50 +0545

    ok

---
+++ javascript.js
@@ -0,0 +1,33 @@
+"use strict";  //treat ypur code according to newer version of js
+
+let name = undefined
+console.log(typeof name) // check the type of variable , 
+
+
+
+                if(name!= undefined){
+                    console.log("name is assigned")
+                } else{
+                    console.log(typeof name)
+                }
+
+
+
+// conversion 
+// Number() 
+// String()
+// Boolen()
+
+
+
+
+let age = "18";
+console.log(typeof age)
+console.log(typeof Number(age))
+
+
+// 1= true   || 0 = false || "noEmpty"= true || "" = False
+let isLoggedIn = 1;
+let boolenStatus = Boolean(isLoggedIn);
+console.log(boolenStatus);
+
