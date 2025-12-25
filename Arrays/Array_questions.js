9a46654c7da3238ed898311be0c5d23a25efef03
Ankit Thapa <thapaankit444@gmail.com>
2025-12-25 11:10:41 +0545

    array

---
+++ Arrays/Array_questions.js
@@ -0,0 +1,19 @@
+// create array with 3 fruits and print 2 one.
+
+let fruits = ["Apple", "Mango", "Banana"];
+
+console.log(fruits[1]);
+
+//Add pineapple at beginning and Grapes at end.
+
+
+// fruits.unshift("Pineapple");
+// fruits.push("Grapes")
+// console.log(fruits);
+
+// Replace Mango with Kivi
+
+fruits[1]="Kivi"
+console.log(fruits);
+
+
---
+++ Arrays/Script.js
@@ -0,0 +1,72 @@
+let name = ["Ankit", "Ankush", "Abishek"];
+// console.log(name[1]);
+// // modify arr
+// name[1] = "Amit";
+
+// console.log(name[1]);
+
+
+//sort array
+
+
+// console.log(num);
+
+// num.shift() remove value from 0th index
+
+// console.log(num);
+// num.unshift(99) add value to 0th index 
+// console.log(num);
+
+
+// num.sort(function(a,b){
+    //     return b-a;
+    // })
+    // a-b for ascending and b-a for descending
+    // a-b for ascending and b-a for descending
+    // a-b for ascending and b-a for descending
+    // console.log(num);
+    
+    
+    // console.log(num);
+    
+    // console.log(num.splice(3,3));
+    // it remove item from 3rd index and remove 3 element
+    // console.log(num);
+    
+    
+    // console.log(num);
+    // let newNum = num.slice(1,4)
+    //  it dont change original value
+    // it exclude last index , till last-1, 4th-1
+    // console.log(newNum);
+    
+    
+    let num = [1,4,6,4,32,0]
+// console.log(num.slice(1,4));
+// console.log(num);
+
+
+// let mappedArray = num.map(function(value){
+//     // let a= value;
+//     return value*2;
+// })
+// console.log(mappedArray);
+
+// let filterArray= num.filter(function(values){
+
+//     if(values>6){ return false;}
+//     else{return true}
+// })
+// console.log(filterArray);
+
+// console.log(num);
+
+let findArr= num.find(function(value){
+    return value === 4
+})
+
+console.log(findArr);
+
+
+
+
